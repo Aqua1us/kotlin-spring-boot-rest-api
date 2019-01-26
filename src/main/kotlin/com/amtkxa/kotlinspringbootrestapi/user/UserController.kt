4fbdcc2fdbbd8f6dfabdfcaeb669cf44de3b15f4
amtkxa <scarlet0211@outlook.jp>
2019-01-26 11:06:26 +0900

    findById を追加

--- src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserController.kt
+++ src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserController.kt
@@ -2,12 +2,19 @@ package com.amtkxa.kotlinspringbootrestapi.user
 
 import lombok.NonNull
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserController(@NonNull private val userService: UserService) {
 
     @GetMapping(path = ["/api/user"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
-    fun findAll(): List<User> = userService.findAll()
+    fun findAll(): List<User> =
+        userService.findAll()
+
+    @GetMapping(path = ["/api/user/{id}"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+    fun findById(@PathVariable(value = "id") id: Long): ResponseEntity<User> =
+        userService.findById(id = id)
 }
--- src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserService.kt
+++ src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserService.kt
@@ -2,11 +2,19 @@ package com.amtkxa.kotlinspringbootrestapi.user
 
 import lombok.NonNull
 import lombok.RequiredArgsConstructor
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
 @RequiredArgsConstructor
 class UserService(@NonNull private val userRepository: UserRepository) {
 
-    fun findAll(): List<User> = userRepository.findAll()
+    fun findAll(): List<User> =
+        userRepository.findAll()
+
+    fun findById(id: Long): ResponseEntity<User> =
+        userRepository.findById(id).map { user ->
+            ResponseEntity.ok(user)
+        }.orElse(ResponseEntity.notFound().build())
+
 }

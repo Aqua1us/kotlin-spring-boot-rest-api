3f1ac46e142612f904940b2db500831e4c610ae9
amtkxa <scarlet0211@outlook.jp>
2019-01-28 00:06:26 +0900

    create を追加

--- src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserController.kt
+++ src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserController.kt
@@ -3,18 +3,28 @@ package com.amtkxa.kotlinspringbootrestapi.user
 import lombok.NonNull
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class UserController(@NonNull private val userService: UserService) {
 
     @GetMapping(path = ["/api/user"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
-    fun findAll(): List<User> =
-        userService.findAll()
+    fun findAll(): ResponseEntity<List<User>> {
+        return ResponseEntity.ok(userService.findAll())
+    }
 
     @GetMapping(path = ["/api/user/{id}"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
-    fun findById(@PathVariable(value = "id") id: Long): ResponseEntity<User> =
-        userService.findById(id = id)
+    fun findById(@PathVariable(value = "id") id: Long): ResponseEntity<User> {
+        val user = userService.findById(id = id)
+        return if (user != null) {
+            ResponseEntity.ok(user)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @PostMapping(path = ["/api/user"], produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+    fun create(@RequestBody user: User): ResponseEntity<User> {
+        return ResponseEntity.ok(userService.create(user))
+    }
 }
--- src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserService.kt
+++ src/main/kotlin/com/amtkxa/kotlinspringbootrestapi/user/UserService.kt
@@ -2,19 +2,24 @@ package com.amtkxa.kotlinspringbootrestapi.user
 
 import lombok.NonNull
 import lombok.RequiredArgsConstructor
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 @RequiredArgsConstructor
 class UserService(@NonNull private val userRepository: UserRepository) {
 
-    fun findAll(): List<User> =
-        userRepository.findAll()
+    @Transactional(readOnly = true)
+    fun findAll(): List<User> {
+        return userRepository.findAll()
+    }
 
-    fun findById(id: Long): ResponseEntity<User> =
-        userRepository.findById(id).map { user ->
-            ResponseEntity.ok(user)
-        }.orElse(ResponseEntity.notFound().build())
+    @Transactional(readOnly = true)
+    fun findById(id: Long): User? {
+        return userRepository.findById(id).orElseGet(null)
+    }
 
+    fun create(user: User): User {
+        return userRepository.save(user)
+    }
 }
